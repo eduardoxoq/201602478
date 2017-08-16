dc04a3fa980ea57d290c67a585e4867be76b8c7d
Eduardo <Eduardo@192.168.0.15>
2017-08-15 18:20:38 -0600

    practica 1

---
+++ src/buscaminas/pkg201602478/menu.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas.pkg201602478;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class menu {
+    public void menuprincipal(){
+        int opcion;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("...................................................");
+        System.out.println("                  ¡BUSCAMINAS!");
+        System.out.println("                1.Principiante");
+        System.out.println("                2.Intermedio");
+        System.out.println("                3.Experto");
+        System.out.println("                4.Salir");
+        System.out.println("...................................................");
+        //System.out.println("");
+        System.out.println("Ingrese opcion");
+        opcion = in.nextInt();
+        
+    
+}
+ 
+    
+    
+}
